532ec0fa6e6f90921dfce2de2aad75b9a62f26fc
BradHuang <adghbc01@tamu.edu>
2018-09-01 23:15:03 +0000

    mongoDB & server setup.

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    mongoURI: "mongodb://brad:brad123@ds141942.mlab.com:41942/mernshoppinglistapp",
+};
---
+++ routes/api/items.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const router = express.Router();
+
+const Item = require("../../models/Item");
+
+// GET api/items get all items
+router.get("/", (req, res) => {
+    Item.find()
+        .sort({ date: -1 })
+        .then(items => res.json(items));
+});
+// TypeError: Cannot read property 'get' of undefined
+// This error is because of the old version of express. 
+
+// POST api/items create an item
+router.post("/", (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+    });
+    newItem
+        .save()
+        .then(item => res.json(item));
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
