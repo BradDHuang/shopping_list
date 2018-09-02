d7ce5e1259bb5ba76dfb821ad3136e4c65732ed7
BradHuang <adghbc01@tamu.edu>
2018-09-02 20:22:00 +0000

    store & reducers & actions

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,8 +3,12 @@ import React, {Component} from "react";
 import { Container, ListGroup, ListGroupItem, Button} from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
+import { connect } from "react-redux";
+import { getItems } from "../actions/itemActions";
+import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
+    /*
     constructor(props) {
         super(props);
         this.state = {
@@ -16,8 +20,13 @@ class ShoppingList extends Component {
             ]
         };
     }
+    */
+    componentDidMount() {
+        this.props.getItems();
+    }
     render() {
-        const { items } = this.state;
+        // const { items } = this.state;
+        const { items } = this.props.item;
         return (
             <Container>
                 <Button
@@ -63,7 +72,16 @@ class ShoppingList extends Component {
     }
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    item: state.item,
+});
+
+export default connect(mapStateToProps, { getItems })(ShoppingList);
 
 
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,16 @@
+
+import {combineReducers} from 'redux';
+import itemReducer from './itemReducer';
+
+const reducers = combineReducers({
+  item: itemReducer,
+});
+
+export default reducers;
+
+
+
+
+
+
+
